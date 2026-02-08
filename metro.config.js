84bb807f3357b08feccdc9f9d57bda8bae4e8f21
Jordan Muse <jordan.m.muse@gmail.com>
2026-02-08 03:59:04 -0800

    Remove Rork and standardize on npm/Expo

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,6 @@
+// metro.config.js
 const { getDefaultConfig } = require("expo/metro-config");
-const { withRorkMetro } = require("@rork-ai/toolkit-sdk/metro");
 
 const config = getDefaultConfig(__dirname);
 
-module.exports = withRorkMetro(config);
+module.exports = config;
